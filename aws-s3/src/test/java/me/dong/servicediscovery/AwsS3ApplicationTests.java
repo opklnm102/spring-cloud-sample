364380915bcdb8be8c21d7ea094e6e12145d2f56
Hue Kim <opklnm102@gmail.com>
2019-11-06 01:08:00 +0900

    [Add] Add aws-s3 module

---
+++ aws-s3/src/test/java/me/dong/servicediscovery/AwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dong.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AwsS3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
